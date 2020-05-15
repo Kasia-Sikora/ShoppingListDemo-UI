5262f814643b7529b07667ae6b61bd2bd558d253
Kasia-Sikora <sikora.katarzyna.waw@gmail.com>
2020-05-15 17:21:38 +0200

    Modal size fit to content

--- src/app/home/navbarComponent/navbar.component.ts
+++ src/app/home/navbarComponent/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ModalService} from '../../utils/modal';
 import {AuthorisationService} from '../../utils/forms/authorisation.service';
 import {Router} from '@angular/router';
+import {HttpClient, HttpResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-navbar',
@@ -13,7 +14,7 @@ export class NavbarComponent implements OnInit {
   pageTitle = 'Stuff';
 
   constructor(private modalService: ModalService, private authorisationService: AuthorisationService,
-              private router: Router) {
+              private router: Router, private http: HttpClient) {
   }
 
   openModal(id: string) {
@@ -30,6 +31,12 @@ export class NavbarComponent implements OnInit {
 
   // TODO one user have to refresh whole page after logout to login another user
   logout() {
+    this.http.post('http://localhost:8080/logout', {observe: 'response'}).subscribe(
+      (response: HttpResponse<any>) => {
+        if (response != null) {
+          console.log(response);
+        }
+      });
     this.authorisationService.setUser(null);
     this.authorisationService.setToken(null);
     this.router.navigate(['']);
